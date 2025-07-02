61dd377c6c879ca42b4947afb86bc23a7c5eb5b0
kks1177 <kks1177.dev@gmail.com>
2025-07-02 17:10:17 +0900

    Java Study

---
+++ Java/ThisIsJava/ch06/Ex6_Car2.java
@@ -0,0 +1,13 @@
+// Ex6_Car2.java
+// 필드 선언과 사용
+
+package ThisIsJava.ch06;
+
+public class Ex6_Car2 {
+    // 필드 선언
+    String company = "현대자동차";
+    String model = "그랜져";
+    String color = "검정";
+    int maxSpeed = 350;
+    int speed = 0;
+}
---
+++ Java/ThisIsJava/ch06/Ex6_CarExam2.java
@@ -0,0 +1,22 @@
+// Ex6_CarExam2.java
+// 필드 선언과 사용
+
+package ThisIsJava.ch06;
+
+public class Ex6_CarExam2 {
+    public static void main(String[] args) {
+        // Car 객체 생성
+        Ex6_Car2 myCar = new Ex6_Car2();
+
+        // Car 객체의 필드값 읽기
+        System.out.println("제작회사 : " + myCar.company);
+        System.out.println("모델명 : " + myCar.model);
+        System.out.println("색깔 : " + myCar.color);
+        System.out.println("최고속도 : " + myCar.maxSpeed);
+        System.out.println("현재속도 : " + myCar.speed);
+
+        // Car 객체의 필드값 변경
+        myCar.speed = 60;
+        System.out.println("수정된 속도 : " + myCar.speed);
+    }
+}
