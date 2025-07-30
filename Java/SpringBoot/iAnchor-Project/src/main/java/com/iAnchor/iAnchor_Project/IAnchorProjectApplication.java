4e239920829ded89ce85f0cd41031e3cd580d25a
kks1177 <kks1177.dev@gmail.com>
2025-07-30 16:43:30 +0900

    SpringBoot Study

---
+++ Java/SpringBoot/iAnchor-Project/src/main/java/com/iAnchor/iAnchor_Project/IAnchorProjectApplication.java
@@ -0,0 +1,13 @@
+package com.iAnchor.iAnchor_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IAnchorProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IAnchorProjectApplication.class, args);
+	}
+
+}
---
+++ Java/SpringBoot/iAnchor-Project/src/test/java/com/iAnchor/iAnchor_Project/IAnchorProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iAnchor.iAnchor_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IAnchorProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
