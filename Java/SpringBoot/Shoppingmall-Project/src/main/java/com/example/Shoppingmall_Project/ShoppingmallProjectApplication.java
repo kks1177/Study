705f19148365c76249169946f8e1446fc621a8c2
kks1177 <kks1177@gmail.com>
2025-07-31 23:06:48 +0900

    SpringBoot Study

--- Java/SpringBoot/Shoppingmall-Project/src/main/java/com/example/Shoppingmall_Project/ShoppingmallProjectApplication.java
+++ Java/SpringBoot/Shoppingmall-Project/src/main/java/com/example/Shoppingmall_Project/ShoppingmallProjectApplication.java
@@ -2,7 +2,10 @@ package com.example.Shoppingmall_Project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class ShoppingmallProjectApplication {
 
@@ -10,4 +13,8 @@ public class ShoppingmallProjectApplication {
 		SpringApplication.run(ShoppingmallProjectApplication.class, args);
 	}
 
+	@GetMapping(value = "/")
+	public String HelloWorld(){
+		return "Hello World";
+	}
 }
