81e9c518329685a17b09f8038e73085209601189
kks1177 <kks1177.dev@gmail.com>
2025-08-01 10:27:49 +0900

    250801_SpringBoot Study

---
+++ Java/SpringBoot/Shoppingmall-Demo/src/test/java/com/example/Shoppingmall_Demo/ShoppingmallDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Shoppingmall_Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingmallDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Java/SpringBoot/Shoppingmall-Project/src/test/java/com/shop/Shoppingmall_Project/ShoppingmallProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.Shoppingmall_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingmallProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
