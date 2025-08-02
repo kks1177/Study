f990001d6b60a741cf24d3e88e261f754eca3420
kks1177 <kks1177@gmail.com>
2025-08-03 01:43:50 +0900

    250803_SpringBoot Study

--- Java/SpringBoot/shoppingmall/src/main/java/com/shoppingmall/repository/ItemRepository.java
+++ Java/SpringBoot/shoppingmall/src/main/java/com/shoppingmall/repository/ItemRepository.java
@@ -8,5 +8,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
-    List<Item> findByItemNum(String itemNm);
+    // p.81 [함께 해봐요 2-3] 상품 Repository 작성 및 테스트 하기
+
+    // p.86 [함께 해봐요 2-4] 쿼리 메소드를 이용한 상품 조회하기
+    List<Item> findByItemNm(String itemNm);
+
+    // p.90 [함께 해봐요 2-5] OR 조건 처리하기
+    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
+
+    // p.91 [함께 해봐요 2-6] LessThan 조건 처리하기
+    List<Item> findByPriceLessThan(Integer price);
 }
--- Java/SpringBoot/shoppingmall/src/test/java/com/shoppingmall/repository/ItemRepositoryTest.java
+++ Java/SpringBoot/shoppingmall/src/test/java/com/shoppingmall/repository/ItemRepositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,7 +19,9 @@ class ItemRepositoryTest {
     @Autowired
     ItemRepository itemRepository;
 
-    @Test@DisplayName("상품 저장 테스트")
+    // p.81 [함께 해봐요 2-3] 상품 Repository 작성 및 테스트 하기
+    @Test
+    @DisplayName("상품 저장 테스트")
     public void createItemTest() {
         Item item = new Item();
         item.setItemNm("테스트 상품");
@@ -30,5 +33,54 @@ class ItemRepositoryTest {
         item.setUpdateTime(LocalDateTime.now());
         Item savedItem = itemRepository.save(item);
         System.out.println(savedItem.toString());
+    }
+
+    // p.86 [함께 해봐요 2-4] 쿼리 메소드를 이용한 상품 조회하기
+    public void createItemList() {
+        for (int i=1; i<=10; i++) {
+            Item item = new Item();
+            item.setItemNm("테스트 상품" + i);
+            item.setPrice(10000 + i);
+            item.setItemDetail("테스트 상품 상세 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SELL);
+            item.setStockNumber(100);
+            //item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+            Item savedItem = itemRepository.save(item);
+        }
+    }
+    @Test
+    @DisplayName("상품명 조회 테스트")
+    public void findByItemNmTest() {
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemNm("테스트 상품1");
+
+        for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    // p.90 [함께 해봐요 2-5] OR 조건 처리하기
+    @Test
+    @DisplayName("상품명, 상품상세설명 or 테스트")
+    public void findByItemNmOrItemDetailTest() {
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemNmOrItemDetail("테스트 상품1", "테스트 상품 상세 설명5");
+
+        for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    // p.91 [함께 해봐요 2-6] LessThan 조건 처리하기
+    @Test
+    @DisplayName("가격 LessThan 테스트")
+    public void findByPriceLessThanTest() {
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByPriceLessThan(10005);
+
+        for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
     }
 }
